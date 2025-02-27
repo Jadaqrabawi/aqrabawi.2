2c2d6743166ee400518abbe74e9fb08097659e17
jadaqrabawi <jadaqrabawi7@gmail.com>
2025-02-27 13:48:40 +0300

    oss

--- oss.c
+++ oss.c
@@ -13,11 +13,9 @@
 
  #include <stdio.h>      
  #include <stdlib.h>     
- #include <unistd.h>     
- #include <sys/shm.h>    
- #include <sys/ipc.h>    
- #include <sys/types.h>  
- #include <sys/wait.h>   
+ #include <unistd.h>        
+ #include <sys/types.h> 
+ #include <sys/wait.h>
  #include <signal.h>     
  #include <time.h>       
  #include <string.h>     
