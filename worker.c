1bd003a07caf78a0ac8a787ec3cc9301b6321c4c
jadaqrabawi <jadaqrabawi7@gmail.com>
2025-02-27 13:49:40 +0300

    worker

--- worker.c
+++ worker.c
@@ -10,9 +10,7 @@
 
  #include <stdio.h>      
  #include <stdlib.h>     
- #include <unistd.h>     
- #include <sys/shm.h>    
- #include <sys/ipc.h>    
+ #include <unistd.h>       
  #include <signal.h>     
  #include <stdbool.h>    
  
